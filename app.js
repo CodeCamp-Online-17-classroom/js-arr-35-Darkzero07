c499986ec05fc87b56995d312293aba046cccc71
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-21 09:01:09 +0700

    js lab 121

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// write code here
+// ให้สร้าง array result จาก array ที่กำหนด โดยใช้ arr.map(fn)
+
+const array = [
+  { name: "apple", birth: "2000-01-01" },
+  { name: "banana", birth: "1990-10-01" },
+  { name: "watermelon", birth: "1985-12-01" },
+];
+const calAge = (birthDate) => {
+  const today = new Date();
+  const birth = new Date(birthDate);
+  const age = today.getFullYear() - birth.getFullYear();
+  return age;
+};
+// const array2 = array.map((arr) => ({ ...arr, age: calAge(arr.birth) -3 }));
+const array2 = array.map((arr) => ({ ...arr, age: calAge(arr.birth) }));
+console.log(array2);
+
+// result: [
+//   { name: "apple", birth: "2000-01-01", age: 21 },
+//   { name: "banana", birth: "1990-10-01", age: 31 },
+//   { name: "watermelon", birth: "1985-12-01", age: 36 },
+// ]
